c53577f494413103993ac250ed2d43873711d091
yk_glb <1523033024@qq.com_glb>
2021-10-27 14:53:43 +0800

    统一下单api

--- src/main/kotlin/com/example/WxPayController.kt
+++ src/main/kotlin/com/example/WxPayController.kt
@@ -2,7 +2,9 @@ package com.example
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.github.wxpay.sdk.WXPay
 import com.github.wxpay.sdk.WXPayConfig
+import com.github.wxpay.sdk.WXPayConstants
 import org.slf4j.LoggerFactory
 import java.io.ByteArrayInputStream
 import java.io.File
@@ -10,10 +12,7 @@ import java.io.FileInputStream
 import java.io.InputStream
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
+import javax.ws.rs.*
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.core.MediaType
 import kotlin.concurrent.timerTask
@@ -22,12 +21,54 @@ import kotlin.concurrent.timerTask
 @Path("/WxPay")
 class WxPayController{
 
-    val executorService: ExecutorService = Executors.newCachedThreadPool()
-    val client = ClientBuilder.newBuilder().executorService(executorService).build()
 
+    //get pay config
+    @GET
+    @Path("/GetConfig")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun GetPayConfig(
+        @QueryParam("url") url: String
+    ): String {
+        val mapper = ObjectMapper()
+        val jsapi_ticket = "LIKLckvwlJT9cWIhEQTwfDvK5RXd_GUYN8YIKxB6WceqZbpaS_Kiy5q3dkE5gkRhysoq1WhtHQk8Ktf0EN0dtQ"
+        val map = Sign.sign(jsapi_ticket, url)
+        return mapper.writeValueAsString(map)
+    }
 
+    @POST
+    @Path("/GetSign")
+    fun GetPaySign(): String{
+//        val config = MyConfig()
+//        val wxPay = WXPay(config, WXPayConstants.SignType.HMACSHA256);
+//        val map: MutableMap<String, String> = mutableMapOf("appid" to config.appID, "mch_id" to config.mchID, "nonce_str" to )
+//
+//
+//
+//        return ""
 
+        val config = MyConfig()
+        val wxpay = WXPay(config, WXPayConstants.SignType.HMACSHA256)
 
+        val data: MutableMap<String, String> = HashMap()
+        data["body"] = "腾讯充值中心-QQ会员充值"
+        data["out_trade_no"] = "2016090910595900000012"
+        data["fee_type"] = "CNY"
+        data["total_fee"] = "1"
+        data["spbill_create_ip"] = "123.12.12.123"
+        data["notify_url"] = "http://www.example.com/wxpay/notify"
+        data["trade_type"] = "JSAPI" // 此处指定为扫码支付
+
+        data["openid"] = "o_NIi5jNCHccjjCUJ2Ut3ExfWFUc"
+
+        try {
+            val resp = wxpay.unifiedOrder(data)
+            logger.info(resp.toString())
+            println(resp)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return ""
+    }
 
     @GET
     @Path("/getOpenId")
@@ -47,6 +88,9 @@ class WxPayController{
 
     //get openid and remote access_token
     fun getOpenIdRemote(code: String){
+
+        val executorService: ExecutorService = Executors.newCachedThreadPool()
+        val client = ClientBuilder.newBuilder().executorService(executorService).build()
         val endpoint = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$AppId&secret=$AppSecret&code=$code&grant_type=authorization_code"
         val res = client.target(endpoint)
             .request()
@@ -59,6 +103,8 @@ class WxPayController{
 
     //todo: refresh access_token
     fun refreshAccessToken(){
+        val executorService: ExecutorService = Executors.newCachedThreadPool()
+        val client = ClientBuilder.newBuilder().executorService(executorService).build()
         val refresh_token = Session.cache.get("refresh_token")
         if(!refresh_token.isNullOrEmpty()){
             val endpoint = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=$AppId&grant_type=refresh_token&refresh_token=$refresh_token"
@@ -77,6 +123,8 @@ class WxPayController{
 
     //todo: get jsapi_ticket
     fun getJsApiTicket(access_token: String){
+        val executorService: ExecutorService = Executors.newCachedThreadPool()
+        val client = ClientBuilder.newBuilder().executorService(executorService).build()
         val endpoint = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$access_token&type=jsapi"
         val res = client.target(endpoint)
             .request()
@@ -85,18 +133,6 @@ class WxPayController{
         Session.cache.put("ticket", entity.get("ticket").toString())
     }
 
-    //get pay config
-    @GET
-    @Path("/GetPayConfig")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun GetPayConfig(
-        @QueryParam("url") url: String
-    ): String {
-        val mapper = ObjectMapper()
-        val jsapi_ticket = "LIKLckvwlJT9cWIhEQTwfDvK5RXd_GUYN8YIKxB6WceqZbpaS_Kiy5q3dkE5gkRhysoq1WhtHQk8Ktf0EN0dtQ"
-        val map = Sign.sign(jsapi_ticket, url)
-        return mapper.writeValueAsString(map)
-    }
 
 
     companion object{
@@ -124,7 +160,7 @@ class MyConfig : WXPayConfig {
     }
 
     override fun getKey(): String {
-        return "ce24463682d67dcea1ae7b1d3015ab38"
+        return "dfg8786ghf78hjg09hjkliuytrdfghjl"
     }
 
     override fun getCertStream(): InputStream {
@@ -141,7 +177,7 @@ class MyConfig : WXPayConfig {
 
     init {
         //todo: download cert
-        val certPath = "/path/to/apiclient_cert.p12"
+        val certPath = "D:\\code\\idea\\workspace_1\\wepay-demo\\src\\main\\resources\\META-INF\\resources\\path\\to\\apiclient_cert.p12"
         val file = File(certPath)
         val certStream: InputStream = FileInputStream(file)
         certData = ByteArray(file.length().toInt())
